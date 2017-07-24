232714f4aa714f52e7e6997dec05e21d93851903
adwit009 <vasu6895@gmail.com>
2017-07-23 17:25:22 -0700

    Add files via upload

--- src/day1/ForLoopGauntlet.java
+++ src/day1/ForLoopGauntlet.java
@@ -6,7 +6,7 @@ import com.spun.util.database.SQLQuery.FromPart;
 
 public class ForLoopGauntlet {
 public static void main(String[] args) {
-c4();
+c11();
 }
 public static void c1(){
 for (int i = 0; i < 101; i++) {
@@ -48,12 +48,98 @@ for (int i = 1; i < 10; i = i +2 ) {
 System.out.println(i);
 }
 }
+public static void c8(){
+int b = 0;
+	for (int i = 0; i <3; i ++) {
+
+	for(int a =0; a<3; a ++){
+System.out.println(b + " "+ a);
+	}
+b = b+ 1;	
+}
+
+}
+
+
+
+
+
+
+
+
+
+
 public static void c6(){
-for (int i = 1; i < 10; i = i +2 ) {
+for (int i = 0; i < 778; i = i +7 ) {
 System.out.println(i);
+
 }
 }
+public static void c9(){
+int a = 1;
+int b = 2;
+int c = 3;
+	for (int i = 0; i < 3;  i ++ ) {
+System.out.println(a + " "+ b + " " + c);
+a = a + 3;
+b = b+ 3;
+c = c + 3;
+
 }
+}
+
+
+
+public static void c10(){
+int x = 0;
+	for (int i = 1; i < 11; i++) {
+	System.out.println();
+	
+	for (int j = 1 + x; j < 11 + x; j++) {
+
+		System.out.print(j + " ");	
+	}
+x = x + 10;
+	}
+	
+}
+
+public static void c11(){
+int x = 0;
+	for (int i = 1; i < 7; i++) {
+	System.out.println();
+	
+	for (int j = 1 ; j < 2 + x; j++) {
+
+		System.out.print("*");	
+	}
+x = x + 1;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
 
 
 
