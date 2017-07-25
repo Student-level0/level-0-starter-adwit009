c99b6ab3878081b60f147d8567b44a9eabde4255
adwit009 <vasu6895@gmail.com>
2017-07-24 20:43:59 -0700

    Add files via upload

---
+++ BoringWindow.java
@@ -0,0 +1,15 @@
+package day1;
+import javax.swing.*;
+public class BoringWindow extends JFrame {
+
+
+	public static void main(String[] args) {
+		JFrame f = new BoringWindow();
+		f.setTitle("hi");
+		f.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		f.setSize(500, 500);
+		f.setVisible(true);
+		f.getContentPane();
+	
+	}
+}
---
+++ HarmonicMean.java
@@ -0,0 +1,16 @@
+package day1;
+
+import java.util.Scanner;
+
+public class HarmonicMean {
+public static void main(String[] args) {
+	int x;
+	Scanner keyboard = new Scanner(System.in);
+
+x = keyboard.nextInt();
+	for (int n = 0; n < x; n++) {
+		
+	}
+}
+}
+
