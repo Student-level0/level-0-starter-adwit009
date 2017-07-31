7fafd02ef91635e801cf860c45389b27c0ff2490
adwit009 <vasu6895@gmail.com>
2017-07-30 17:31:39 -0700

    Add files via upload

---
+++ src/day1/SimpleNumberSorter.java
@@ -0,0 +1,30 @@
+package day1;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class SimpleNumberSorter {
+public static void main(String[] args) {
+	Scanner keyboard = new Scanner(System.in);
+	System.out.println("Enter different 3 numbers (clicking enter after each one)");
+double a;
+double b;
+double c;
+a= keyboard.nextDouble(); 
+b=keyboard.nextDouble() ;
+c=keyboard.nextDouble() ;
+int a1 = 0;
+int b1= 5;
+int c1 = 10;
+
+if(a>b) {
+	a1 = 6;
+}
+if (a>c) {
+	a1 = a1+6;
+}
+if (b>c) {
+	
+}
+}
+}
